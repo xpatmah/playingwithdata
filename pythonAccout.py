020ba3b2b0d0fbdb89506c2ea12bfc16c15b15ef
xpatmah <mchpathak@gmail.com>
2019-03-03 12:39:21 +0530

    Beginners python

---
+++ pythonAccout.py
@@ -0,0 +1,23 @@
+class Account:
+
+    def __init__(self, balance, account_name):
+        self.balance = balance
+        self.account_name = account_name
+
+    def __str__(self):
+        return self.balance + ' ' + self.account_name
+
+    def withdrawl(self, amount):
+        if amount < self.balance:
+            self.balance -= amount
+            return 'WithDrawl Successful for amount {0}'.format(amount)
+        else:
+            return 'WithDrawl unsuccessful'
+
+    def deposit(self, amount):
+        self.balance += amount;
+        return 'deposit successful for the amount {0}'.format(amount)
+
+
+acount = Account(500, 'Saving Account')
+print(acount.withdrawl(300))
